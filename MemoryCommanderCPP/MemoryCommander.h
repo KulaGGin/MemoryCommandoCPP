22116e458529bb9ce93a5dddc5e3749018ef66c1
KulaGGin <sergeant.coolagin@gmail.com>
2020-06-19 15:54:14 +0300

    Add starting solution files.

---
+++ MemoryCommanderCPP/MemoryCommander.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <vector>
+#include <windows.h>
+#include <TlHelp32.h>
+
+
+namespace MemoryCommanderCpp {
+    class MemoryCommander {
+    };
+}
---
+++ MemoryCommanderCPP/MemoryManager.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <vector>
+#include <windows.h>
+
+class MemoryManager {
+};
