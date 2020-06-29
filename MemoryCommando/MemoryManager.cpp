1384defe89fc9d3c5f3be6ed383d7f5cefd1c527
KulaGGin <sergeant.coolagin@gmail.com>
2020-06-29 13:09:23 +0300

    Fill external MemoryManager class.

--- MemoryCommando/MemoryManager.cpp
+++ MemoryCommando/MemoryManager.cpp
@@ -1,60 +1,168 @@
 #include "MemoryManager.h"
-
-#include <locale>
-#include <Psapi.h>
-#include "ProcessAccess.h"
-#include "CreateToolhelp32SnapshotException.h"
-#include "Process32Exception.h"
-#include <wil/resource.h>
-#include <boost/locale.hpp>
-#include <boost/locale/conversion.hpp>
-#include <boost/algorithm/string.hpp>
-#include "Module32Exception.h"
-
 #include "External.h"
 
 namespace MemoryCommando::External {
-    using namespace Exceptions;
-    namespace conv = boost::locale::conv;
-    namespace algorithm = boost::algorithm;
-    namespace locale = boost::locale;
-
     MemoryManager::MemoryManager(DWORD processId, DWORD processAccess) {
-        _processHandle = GetProcessHandle(processId, processAccess);
-        _processId = GetProcessId(_processHandle);
-        _processName = GetProcessName(_processHandle);
+        _process = External::GetProcess(processId);
+        _processHandle = External::GetProcessHandle(processId, processAccess);
     }
 
     MemoryManager::MemoryManager(const std::wstring& processName, size_t processNumber, DWORD processAccess) {
-        _processHandle = GetProcessHandle(processName, processNumber, processAccess);
-        _processId = GetProcessId(_processHandle);
-        _processName = GetProcessName(_processHandle);
-    }
-
-    MemoryManager::MemoryManager(const std::string& processName, size_t processNumber, DWORD processAccess) {
-        const std::wstring processNameWide = conv::utf_to_utf<WCHAR>(processName);
-        _processHandle = GetProcessHandle(processNameWide, processNumber, processAccess);
-        _processId = GetProcessId(_processHandle);
-        _processName = GetProcessName(_processHandle);
-    }
-
-    std::vector<MODULEENTRY32W> MemoryManager::GetModules() const {
-        return External::GetModules(_processId);
-    }
-
-    MODULEENTRY32W MemoryManager::GetModule(const std::wstring& moduleName) {
-        return External::GetModule(moduleName, _processId);
-    }
-
-    uintptr_t MemoryManager::GetModuleBaseAddress(const std::wstring& moduleName) {
-        return External::GetModuleBaseAddress(moduleName, _processId);;
-    }
-
-    size_t MemoryManager::GetModuleSize(const std::wstring& moduleName) {
-        return External::GetModuleSize(_processId, moduleName);
+        _process = External::GetProcess(processName, processNumber);
+        _processHandle = External::GetProcessHandle(processName, processNumber, processAccess);
     }
 
     PROCESSENTRY32W MemoryManager::GetProcess() {
-        return External::GetProcess(_processId);
+        return _process;
+    }
+
+    DWORD MemoryManager::GetProcessId() {
+        return _process.th32ProcessID;
+    }
+
+    HANDLE MemoryManager::GetProcessHandle() {
+        return _processHandle;
+    }
+
+    std::wstring MemoryManager::GetProcessName() {
+        return _process.szExeFile;
+    }
+
+    std::vector<MODULEENTRY32W> MemoryManager::GetModules() const {
+        return External::GetModules(_process.th32ProcessID);
+    }
+
+    MODULEENTRY32W MemoryManager::GetModule(const std::wstring& moduleName) {
+        return External::GetModule(moduleName, _process.th32ProcessID);
+    }
+
+    uintptr_t MemoryManager::GetModuleBaseAddress(const std::wstring& moduleName) {
+        return External::GetModuleBaseAddress(moduleName, _process.th32ProcessID);;
+    }
+
+    size_t MemoryManager::GetModuleSize(const std::wstring& moduleName) {
+        return External::GetModuleSize(_process.th32ProcessID, moduleName);
+    }
+
+    uintptr_t MemoryManager::AllocateVirtualMemory(uintptr_t baseAddress, size_t allocationSize, DWORD allocationType, DWORD protectionType) {
+        return External::AllocateVirtualMemory(_processHandle, baseAddress, allocationSize, allocationType, protectionType);
+    }
+
+    void MemoryManager::FreeVirtualMemory(uintptr_t baseAddress, DWORD freeType, size_t size) {
+        External::FreeVirtualMemory(_processHandle, baseAddress, freeType, size);
+    }
+
+    void MemoryManager::ProtectVirtualMemory(uintptr_t baseAddress, size_t protectionSize, DWORD protectionType) {
+        External::ProtectVirtualMemory(_processHandle, baseAddress, protectionSize, protectionType);
+    }
+
+    MEMORY_BASIC_INFORMATION MemoryManager::QueryVirtualMemory(uintptr_t baseAddress) {
+        return External::QueryVirtualMemory(_processHandle, baseAddress);
+    }
+
+    std::vector<BYTE> MemoryManager::ReadVirtualMemory(uintptr_t baseAddress, size_t bytesNumber) {
+        return External::ReadVirtualMemory(_processHandle, baseAddress, bytesNumber);
+    }
+
+    std::vector<BYTE> MemoryManager::ReadVirtualMemory(std::vector<uintptr_t> pointers, size_t bytesNumber) {
+        return External::ReadVirtualMemory(_processHandle, pointers, bytesNumber);
+    }
+
+    std::vector<BYTE> MemoryManager::ReadVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> offsets, int bytesNumber) {
+        return External::ReadVirtualMemory(_processHandle, baseAddress, offsets, bytesNumber);
+    }
+
+    std::vector<BYTE> MemoryManager::ReadVirtualMemory(std::wstring moduleName, uintptr_t offset, size_t bytesNumber) {
+        return External::ReadVirtualMemory(_processHandle, moduleName, offset, bytesNumber);
+    }
+
+    std::vector<BYTE> MemoryManager::ReadVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> offsets, size_t bytesNumber) {
+        return External::ReadVirtualMemory(_processHandle, moduleName, offsets, bytesNumber);
+    }
+
+    template <typename TStructure>
+    TStructure MemoryManager::ReadVirtualMemory(uintptr_t baseAddress) {
+        return External::ReadVirtualMemory<TStructure>(_processHandle, baseAddress);
+    }
+
+    template <typename TStructure>
+    TStructure MemoryManager::ReadVirtualMemory(std::vector<uintptr_t> pointers) {
+        return External::ReadVirtualMemory<TStructure>(_processHandle, pointers);
+    }
+
+    template <typename TStructure>
+    TStructure MemoryManager::ReadVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> offsets) {
+        return External::ReadVirtualMemory<TStructure>(_processHandle, baseAddress, offsets);
+    }
+
+    template <typename TStructure>
+    TStructure MemoryManager::ReadVirtualMemory(std::wstring moduleName, uintptr_t offset) {
+        return External::ReadVirtualMemory<TStructure>(_processHandle, moduleName, offset);
+    }
+
+    template <typename TStructure>
+    TStructure MemoryManager::ReadVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> offsets) {
+        return External::ReadVirtualMemory<TStructure>(_processHandle, moduleName, offsets);
+    }
+
+    void MemoryManager::WriteVirtualMemory(uintptr_t baseAddress, std::vector<byte> byteSequence) {
+        External::WriteVirtualMemory(_processHandle, baseAddress, byteSequence);
+    }
+
+    void MemoryManager::WriteVirtualMemory(std::vector<uintptr_t> pointers, std::vector<byte> byteSequence) {
+        External::WriteVirtualMemory(_processHandle, pointers, byteSequence);
+    }
+
+    void MemoryManager::WriteVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> offsets, std::vector<byte> byteSequence) {
+        External::WriteVirtualMemory(_processHandle, baseAddress, offsets, byteSequence);
+    }
+
+    void MemoryManager::WriteVirtualMemory(std::wstring moduleName, uintptr_t offset, std::vector<byte> byteSequence) {
+        External::WriteVirtualMemory(_processHandle, moduleName, offset, byteSequence);
+    }
+
+    void MemoryManager::WriteVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> offsets, std::vector<byte> byteSequence) {
+        External::WriteVirtualMemory(_processHandle, moduleName, offsets, byteSequence);
+    }
+
+    template <typename TStructure>
+    void MemoryManager::WriteVirtualMemory(uintptr_t baseAddress, TStructure structure) {
+        External::WriteVirtualMemory<TStructure>(_processHandle, baseAddress, structure);
+    }
+
+    template <typename TStructure>
+    void MemoryManager::WriteVirtualMemory(std::vector<uintptr_t> pointers, TStructure structure) {
+        External::WriteVirtualMemory<TStructure>(_processHandle, pointers, structure);
+    }
+
+    template <typename TStructure>
+    void MemoryManager::WriteVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> pointers, TStructure structure) {
+        External::WriteVirtualMemory<TStructure>(_processHandle, baseAddress, pointers, structure);
+    }
+
+    template <typename TStructure>
+    void MemoryManager::WriteVirtualMemory(std::wstring moduleName, uintptr_t offset, TStructure structure) {
+        External::WriteVirtualMemory<TStructure>(_processHandle, moduleName, offset, structure);
+    }
+
+    template <typename TStructure>
+    void MemoryManager::WriteVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> offsets, TStructure structure) {
+        External::WriteVirtualMemory<TStructure>(_processHandle, moduleName, offsets, structure);
+    }
+
+    uintptr_t MemoryManager::GetAddress(std::vector<uintptr_t> pointers) {
+        return External::GetAddress(_processHandle, pointers);
+    }
+
+    uintptr_t MemoryManager::GetAddress(uintptr_t baseAddress, std::vector<uintptr_t> pointers) {
+        return External::GetAddress(_processHandle, baseAddress, pointers);
+    }
+
+    uintptr_t MemoryManager::GetAddress(std::wstring moduleName, uintptr_t offset) {
+        return External::GetAddress(_processHandle, moduleName, offset);
+    }
+
+    uintptr_t MemoryManager::GetAddress(std::wstring moduleName, std::vector<uintptr_t> offsets) {
+        return External::GetAddress(_processHandle, moduleName, offsets);
     }
 }
--- MemoryCommando/MemoryManager.h
+++ MemoryCommando/MemoryManager.h
@@ -4,7 +4,6 @@
 #include <string>
 #include <vector>
 
-
 namespace MemoryCommandoTests {
     class MemoryManagerExternalTests;
 }
@@ -15,34 +14,39 @@ namespace MemoryCommando::External {
     public:
         MemoryManager(DWORD processId, DWORD processAccess = PROCESS_ALL_ACCESS);
         MemoryManager(const std::wstring& processName, size_t processNumber = 1, DWORD processAccess = PROCESS_ALL_ACCESS);
-        explicit MemoryManager(const std::string& processName, size_t processNumber = 1, DWORD processAccess = PROCESS_ALL_ACCESS);
 
+        PROCESSENTRY32W GetProcess();
+        DWORD GetProcessId();
+        HANDLE GetProcessHandle();
+        std::wstring GetProcessName();
         std::vector<MODULEENTRY32W> GetModules() const;
         MODULEENTRY32W GetModule(const std::wstring& moduleName);
         uintptr_t GetModuleBaseAddress(const std::wstring& moduleName);
         size_t GetModuleSize(const std::wstring& moduleName);
-        PROCESSENTRY32W GetProcess();
-        uintptr_t AllocateVirtualMemory(uintptr_t baseAddress, size_t allocationSize, DWORD protectionType);
-        uintptr_t AllocateVirtualMemory(size_t allocationSize);
-        void FreeVirtualMemory(uintptr_t baseAddress);
-        DWORD ProtectVirtualMemory(uintptr_t baseAddress, size_t protectionSize, DWORD protectionType);
+        uintptr_t AllocateVirtualMemory(uintptr_t baseAddress, size_t allocationSize, DWORD allocationType = MEM_RESERVE | MEM_COMMIT, DWORD protectionType = PAGE_EXECUTE_READWRITE);
+        void FreeVirtualMemory(uintptr_t baseAddress, DWORD freeType = MEM_RELEASE, size_t size = 0);
+        void ProtectVirtualMemory(uintptr_t baseAddress, size_t protectionSize, DWORD protectionType = PAGE_EXECUTE_READWRITE);
         MEMORY_BASIC_INFORMATION QueryVirtualMemory(uintptr_t baseAddress);
 
-        std::vector<BYTE> ReadVirtualMemory(uintptr_t baseAddress, size_t sequenceSize);
-        std::vector<BYTE> ReadVirtualMemory(std::vector<uintptr_t> pointers, size_t sequenceSize);
-        std::vector<BYTE> ReadVirtualMemory(std::wstring moduleName, uintptr_t pointer, size_t sequenceSize);
-        std::vector<BYTE> ReadVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> pointers, size_t sequenceSize);
+        std::vector<BYTE> ReadVirtualMemory(uintptr_t baseAddress, size_t bytesNumber);
+        std::vector<BYTE> ReadVirtualMemory(std::vector<uintptr_t> pointers, size_t bytesNumber);
+        std::vector<BYTE> ReadVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> offsets, int bytesNumber);
+        std::vector<BYTE> ReadVirtualMemory(std::wstring moduleName, uintptr_t offset, size_t bytesNumber);
+        std::vector<BYTE> ReadVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> offsets, size_t bytesNumber);
         template<typename TStructure> TStructure ReadVirtualMemory(uintptr_t baseAddress);
         template<typename TStructure> TStructure ReadVirtualMemory(std::vector<uintptr_t> pointers);
+        template<typename TStructure> TStructure ReadVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> offsets);
         template<typename TStructure> TStructure ReadVirtualMemory(std::wstring moduleName, uintptr_t offset);
         template<typename TStructure> TStructure ReadVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> offsets);
 
         void WriteVirtualMemory(uintptr_t baseAddress, std::vector<byte> byteSequence);
         void WriteVirtualMemory(std::vector<uintptr_t> pointers, std::vector<byte> byteSequence);
+        void WriteVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> offsets, std::vector<byte> byteSequence);
         void WriteVirtualMemory(std::wstring moduleName, uintptr_t offset, std::vector<byte> byteSequence);
         void WriteVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> offset, std::vector<byte> byteSequence);
         template<typename TStructure> void WriteVirtualMemory(uintptr_t baseAddress, TStructure structure);
         template<typename TStructure> void WriteVirtualMemory(std::vector<uintptr_t> pointers, TStructure structure);
+        template<typename TStructure> void WriteVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> pointers, TStructure structure);
         template<typename TStructure> void WriteVirtualMemory(std::wstring moduleName, uintptr_t offset, TStructure structure);
         template<typename TStructure> void WriteVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> offsets, TStructure structure);
 
@@ -54,9 +58,8 @@ namespace MemoryCommando::External {
 
         // todo Commit, Reserve, Free memory functions that auto free memory on object destruction.
     private:
+        PROCESSENTRY32W _process;
         HANDLE _processHandle = nullptr;
-        DWORD _processId{};
-        std::wstring _processName;
 
         friend class MemoryCommandoTests::MemoryManagerExternalTests;
     };
