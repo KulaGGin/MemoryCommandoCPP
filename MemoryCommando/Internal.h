5eab4186d2f6b8bf217bafe9b215d6e470853d03
KulaGGin <sergeant.coolagin@gmail.com>
2020-07-03 11:17:26 +0300

    Fill Internal namespace with methods.

--- MemoryCommando/Internal.h
+++ MemoryCommando/Internal.h
@@ -10,7 +10,7 @@ namespace MemoryCommando::Internal {
     HANDLE GetProcessHandle();
     std::wstring GetProcessName();
     std::vector<MODULEENTRY32W> GetModules();
-    MODULEENTRY32W GetModule(const std::string& moduleName);
+    MODULEENTRY32W GetModule(const std::wstring& moduleName);
     uintptr_t GetModuleBaseAddress(const std::wstring& moduleName);
     size_t GetModuleSize(const std::wstring& moduleName);
 
@@ -19,28 +19,35 @@ namespace MemoryCommando::Internal {
     void ProtectVirtualMemory(uintptr_t baseAddress, size_t protectionSize, DWORD protectionType = PAGE_EXECUTE_READWRITE);
     MEMORY_BASIC_INFORMATION QueryVirtualMemory(uintptr_t baseAddress);
 
+    std::vector<BYTE> ReadVirtualMemory(uintptr_t address, int bytesNumber);
+    std::vector<BYTE> ReadVirtualMemory(const std::vector<uintptr_t>& pointers, int bytesNumber);
+    std::vector<BYTE> ReadVirtualMemory(uintptr_t baseAddress, const std::vector<uintptr_t>& offsets, int bytesNumber);
+    std::vector<BYTE> ReadVirtualMemory(const std::wstring& moduleName, uintptr_t offset, int bytesNumber);
+    std::vector<BYTE> ReadVirtualMemory(const std::wstring& moduleName, const std::vector<uintptr_t>& offsets, int bytesNumber);
+    // todo ReadVirtualMemory from array of bytes and offset
+
     template<typename TStructure> TStructure ReadVirtualMemory(uintptr_t baseAddress);
     template<typename TStructure> TStructure ReadVirtualMemory(std::vector<uintptr_t> pointers);
-    template<typename TStructure> TStructure ReadVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> offsets);
+    template<typename TStructure> TStructure ReadVirtualMemory(uintptr_t baseAddress, const std::vector<uintptr_t>& offsets);
     template<typename TStructure> TStructure ReadVirtualMemory(const std::wstring& moduleName, uintptr_t offset);
-    template<typename TStructure> TStructure ReadVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> offsets);
+    template<typename TStructure> TStructure ReadVirtualMemory(const std::wstring& moduleName, const std::vector<uintptr_t>& offsets);
     // todo ReadVirtualMemory from array of bytes and offset
 
     void WriteVirtualMemory(uintptr_t address, const std::vector<byte>& byteSequence);
-    void WriteVirtualMemory(std::vector<uintptr_t> pointers, std::vector<byte> byteSequence);
-    void WriteVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> offsets, std::vector<byte> byteSequence);
-    void WriteVirtualMemory(std::wstring moduleName, uintptr_t offset, std::vector<byte> byteSequence);
-    void WriteVirtualMemory(const std::wstring& moduleName, std::vector<uintptr_t> offsets, std::vector<byte> byteSequence);
+    void WriteVirtualMemory(const std::vector<uintptr_t>& pointers, const std::vector<byte>& byteSequence);
+    void WriteVirtualMemory(uintptr_t baseAddress, const std::vector<uintptr_t>& offsets, const std::vector<byte>& byteSequence);
+    void WriteVirtualMemory(const std::wstring& moduleName, uintptr_t offset, const std::vector<byte>& byteSequence);
+    void WriteVirtualMemory(const std::wstring& moduleName, const std::vector<uintptr_t>& offsets, const std::vector<byte>& byteSequence);
     // todo WriteVirtualMemory from array of bytes and offset
 
-    template<typename TStructure> void WriteVirtualMemory(uintptr_t baseAddress, TStructure structure);
-    template<typename TStructure> void WriteVirtualMemory(std::vector<uintptr_t> pointers, TStructure structure);
-    template<typename TStructure> void WriteVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> pointers, TStructure structure);
-    template<typename TStructure> void WriteVirtualMemory(std::wstring moduleName, uintptr_t offset, TStructure structure);
-    template<typename TStructure> void WriteVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> offsets, TStructure structure);
+    template<typename TStructure> void WriteVirtualMemory(uintptr_t baseAddress, const TStructure& structure);
+    template<typename TStructure> void WriteVirtualMemory(const std::vector<uintptr_t>& pointers, const TStructure& structure);
+    template<typename TStructure> void WriteVirtualMemory(uintptr_t baseAddress, const std::vector<uintptr_t>& pointers, const TStructure& structure);
+    template<typename TStructure> void WriteVirtualMemory(const std::wstring& moduleName, uintptr_t offset, const TStructure& structure);
+    template<typename TStructure> void WriteVirtualMemory(const std::wstring& moduleName, const std::vector<uintptr_t>& offsets, const TStructure& structure);
     // todo WriteVirtualMemory from array of bytes and offset
 
-    uintptr_t GetAddress(std::vector<uintptr_t> pointers);
+    uintptr_t GetAddress(const std::vector<uintptr_t>& pointers);
     uintptr_t GetAddress(uintptr_t baseAddress, std::vector<uintptr_t> offsets);
     uintptr_t GetAddress(const std::wstring& moduleName, uintptr_t offset);
     uintptr_t GetAddress(const std::wstring& moduleName, std::vector<uintptr_t> offsets);
