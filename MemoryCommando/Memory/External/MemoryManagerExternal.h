f9735bfa85a24961c8c52056ef2e7fc64ae72348
KulaGGin <sergeant.coolagin@gmail.com>
2020-07-06 21:20:20 +0300

    Add comments to MemoryManagerExternal class.

--- MemoryCommando/Memory/External/MemoryManagerExternal.h
+++ MemoryCommando/Memory/External/MemoryManagerExternal.h
@@ -36,22 +36,28 @@ namespace MemoryCommando::Memory::External {
         std::vector<BYTE> ReadVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> offsets, int bytesNumber);
         std::vector<BYTE> ReadVirtualMemory(std::wstring moduleName, uintptr_t offset, size_t bytesNumber);
         std::vector<BYTE> ReadVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> offsets, size_t bytesNumber);
+        // todo ReadVirtualMemory from array of bytes and offset
+
         template<typename TStructure> TStructure ReadVirtualMemory(uintptr_t baseAddress);
         template<typename TStructure> TStructure ReadVirtualMemory(std::vector<uintptr_t> pointers);
         template<typename TStructure> TStructure ReadVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> offsets);
         template<typename TStructure> TStructure ReadVirtualMemory(std::wstring moduleName, uintptr_t offset);
         template<typename TStructure> TStructure ReadVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> offsets);
+        // todo ReadVirtualMemory from array of bytes and offset
 
         void WriteVirtualMemory(uintptr_t baseAddress, std::vector<byte> byteSequence);
         void WriteVirtualMemory(std::vector<uintptr_t> pointers, std::vector<byte> byteSequence);
         void WriteVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> offsets, std::vector<byte> byteSequence);
         void WriteVirtualMemory(std::wstring moduleName, uintptr_t offset, std::vector<byte> byteSequence);
         void WriteVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> offset, std::vector<byte> byteSequence);
+        // todo WriteVirtualMemory from array of bytes and offset
+
         template<typename TStructure> void WriteVirtualMemory(uintptr_t baseAddress, TStructure structure);
         template<typename TStructure> void WriteVirtualMemory(std::vector<uintptr_t> pointers, TStructure structure);
         template<typename TStructure> void WriteVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> pointers, TStructure structure);
         template<typename TStructure> void WriteVirtualMemory(std::wstring moduleName, uintptr_t offset, TStructure structure);
         template<typename TStructure> void WriteVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> offsets, TStructure structure);
+        // todo WriteVirtualMemory from array of bytes and offset
 
         uintptr_t GetAddress(std::vector<uintptr_t> pointers);
         uintptr_t GetAddress(uintptr_t baseAddress, std::vector<uintptr_t> pointers);
