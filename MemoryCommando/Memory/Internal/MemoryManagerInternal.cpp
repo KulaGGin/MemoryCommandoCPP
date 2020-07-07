bb4e092159193b0187ff6789911facf20b86bc42
KulaGGin <sergeant.coolagin@gmail.com>
2020-07-07 13:37:24 +0300

    Fill MemoryManagerInternal class.
    Add GetProcessId, GetProcessHandle, AllocateVirtualMemory, FreeVirtualMemory, ProtectVirtualMemory, QueryVirtualMemory, ReadVirtualMemory, WriteVirtualMemory methods.

--- MemoryCommando/Memory/Internal/MemoryManagerInternal.cpp
+++ MemoryCommando/Memory/Internal/MemoryManagerInternal.cpp
@@ -1,5 +1,78 @@
 #include "MemoryManagerInternal.h"
+#include <processthreadsapi.h>
+#include <stdexcept>
 
-namespace MemoryCommando::Internal {
 
+
+#include "../../Exceptions/CreateToolhelp32SnapshotException.h"
+#include "../../Exceptions/Module32Exception.h"
+#include "../../Exceptions/VirtualAllocException.h"
+#include "../../Exceptions/VirtualFreeException.h"
+#include "../../Exceptions/VirtualProtectException.h"
+#include "../../Exceptions/VirtualQueryException.h"
+
+namespace MemoryCommando::Memory::Internal {
+
+    MemoryManagerInternal::MemoryManagerInternal() {
+        _processId = MemoryManagerInternal::GetProcessId();
+        _processHandle = wil::unique_handle(MemoryManagerInternal::GetProcessHandle());
+    }
+
+    DWORD MemoryManagerInternal::GetProcessId() {
+        return GetCurrentProcessId();
+    }
+
+    HANDLE MemoryManagerInternal::GetProcessHandle() {
+        return GetCurrentProcess();
+    }
+
+    uintptr_t MemoryManagerInternal::AllocateVirtualMemory(const uintptr_t baseAddress, const size_t allocationSize, const DWORD allocationType, const DWORD protectionType) {
+        LPVOID allocationAddress = VirtualAlloc(LPVOID(baseAddress), allocationSize, allocationType, protectionType);
+
+        if(!allocationAddress)
+            throw Exceptions::VirtualAllocException("VirtualAlloc couldn't allocate memory with error code " + std::to_string(GetLastError()) + ".", GetLastError());
+
+        return uintptr_t(allocationAddress);
+    }
+
+    void MemoryManagerInternal::FreeVirtualMemory(const uintptr_t address, const DWORD freeType, const size_t size) {
+        if(freeType == MEM_RELEASE && size != 0)
+            throw std::invalid_argument("When freeType is MEM_RELEASE, size must be 0.");
+
+        const bool didFree = VirtualFree(LPVOID(address), size, freeType);
+
+        if(!didFree)
+            throw Exceptions::VirtualFreeException("VirtualFree couldn't free memory with error code " + std::to_string(GetLastError()) + ".", GetLastError());
+    }
+
+    void MemoryManagerInternal::ProtectVirtualMemory(const uintptr_t baseAddress, const size_t protectionSize, const DWORD protectionType) {
+        DWORD oldProtection;
+        const bool didProtect = VirtualProtect(LPVOID(baseAddress), protectionSize, protectionType, &oldProtection);
+
+        if(!didProtect)
+            throw Exceptions::VirtualProtectException("VirtualProtect failed to protect memory with the error code " + std::to_string(GetLastError()) + ".", GetLastError());
+    }
+
+    MEMORY_BASIC_INFORMATION MemoryManagerInternal::QueryVirtualMemory(const uintptr_t baseAddress) {
+        MEMORY_BASIC_INFORMATION memoryBasicInformation{};
+
+        const SIZE_T bytesReturned = VirtualQuery(LPVOID(baseAddress), &memoryBasicInformation, sizeof memoryBasicInformation);
+
+        if(!bytesReturned)
+            throw Exceptions::VirtualQueryException("VirtualQuery couldn't query memory and failed with error code " + std::to_string(GetLastError()), GetLastError());
+
+        return memoryBasicInformation;
+    }
+
+    std::vector<BYTE> MemoryManagerInternal::ReadVirtualMemory(const uintptr_t address, const size_t bytesNumber) {
+        std::vector<BYTE> bytesSequence;
+        bytesSequence.resize(bytesNumber, 0);
+        std::memcpy(&bytesSequence[0], LPVOID(address), bytesNumber);
+
+        return bytesSequence;
+    }
+
+    void MemoryManagerInternal::WriteVirtualMemory(const uintptr_t address, const std::vector<byte>& byteSequence) {
+        std::memcpy(LPVOID(address), &byteSequence[0], byteSequence.size());
+    }
 }
--- MemoryCommando/Memory/Internal/MemoryManagerInternal.h
+++ MemoryCommando/Memory/Internal/MemoryManagerInternal.h
@@ -1,7 +1,18 @@
 #pragma once
-namespace MemoryCommando::Internal {
-    class MemoryManagerInternal
-    {
+#include "../MemoryManager.h"
+
+namespace MemoryCommando::Memory::Internal {
+    class MemoryManagerInternal final : public MemoryManager {
+    public:
+        MemoryManagerInternal();
+        DWORD GetProcessId() override;
+        HANDLE GetProcessHandle() override;
+        uintptr_t AllocateVirtualMemory(uintptr_t baseAddress, size_t allocationSize, DWORD allocationType, DWORD protectionType) override;
+        void FreeVirtualMemory(uintptr_t address, DWORD freeType, size_t size) override;
+        void ProtectVirtualMemory(uintptr_t baseAddress, size_t protectionSize, DWORD protectionType) override;
+        MEMORY_BASIC_INFORMATION QueryVirtualMemory(uintptr_t baseAddress) override;
+        std::vector<BYTE> ReadVirtualMemory(uintptr_t address, size_t bytesNumber) override;
+        void WriteVirtualMemory(uintptr_t address, const std::vector<byte>& byteSequence) override;
     };
 }
 
