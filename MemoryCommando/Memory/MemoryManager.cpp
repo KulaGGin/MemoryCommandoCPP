0de7d86461a24e1fcc78aff318b726a5566bf2b6
KulaGGin <sergeant.coolagin@gmail.com>
2020-07-07 13:36:39 +0300

    Refactor MemoryManager base abstract class. Move methods into it.
    Move into it GetSystemInfo, GetRunningProcesses, GetProcess, GetProcessName, GetModules, GetModule, GetModuleBaseAddress, ReadVirtualMemory overloads, WriteVirtualMemory overloads, GetAddress overloads.

--- MemoryCommando/Memory/MemoryManager.cpp
+++ MemoryCommando/Memory/MemoryManager.cpp
@@ -1,8 +1,199 @@
 #include "MemoryManager.h"
 
+#include <boost/algorithm/string/predicate.hpp>
+#include <stdexcept>
+
+#include "../Exceptions/CreateToolhelp32SnapshotException.h"
+#include "../Exceptions/Module32Exception.h"
+#include "../Exceptions/Process32Exception.h"
+
 namespace MemoryCommando::Memory {
+    SYSTEM_INFO MemoryManager::GetSystemInfo() {
+        SYSTEM_INFO systemInfo;
+        ::GetSystemInfo(&systemInfo);
+        return systemInfo;
+    }
+
+    std::vector<PROCESSENTRY32W> MemoryManager::GetRunningProcesses() {
+        std::vector<PROCESSENTRY32W> runningProcesses{};
+        PROCESSENTRY32 process{};
+        process.dwSize = sizeof process;
+
+        const wil::unique_tool_help_snapshot processesSnapshot{ CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0) };
+
+        if(!processesSnapshot)
+            throw Exceptions::CreateToolhelp32SnapshotException("CreateToolhelp32Snapshot failed to create a snapshot.", GetLastError());
+
+        bool copiedToBuffer = Process32First(processesSnapshot.get(), &process);
+        if(!copiedToBuffer)
+            throw Exceptions::Process32Exception("Process32First failed to fill the buffer.", GetLastError());
+
+        do {
+            runningProcesses.push_back(process);
+            copiedToBuffer = Process32Next(processesSnapshot.get(), &process);
+        } while(copiedToBuffer);
+
+        if(!copiedToBuffer && GetLastError() != ERROR_NO_MORE_FILES)
+            throw Exceptions::Process32Exception("Process32Next failed to fill the buffer.", GetLastError());
+
+        return runningProcesses;
+    }
+
+    PROCESSENTRY32W MemoryManager::GetProcess() {
+        std::vector<PROCESSENTRY32W> processes = GetRunningProcesses();
+
+        for(auto currentProcess : processes) {
+            if(_processId == currentProcess.th32ProcessID)
+                return currentProcess;
+        }
+
+        throw std::runtime_error("Couldn't find a process with the specified processId in the process list.");
+    }
+
+    std::wstring MemoryManager::GetProcessName() {
+            const PROCESSENTRY32W process = GetProcess();
+            return process.szExeFile;
+    }
+
+    std::vector<MODULEENTRY32W> MemoryManager::GetModules() const {
+        std::vector<MODULEENTRY32W> modules{};
+        MODULEENTRY32               module{};
+        module.dwSize = sizeof module ;
+
+        const auto modulesSnapshot = wil::unique_tool_help_snapshot(CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, _processId));
+
+        if(!modulesSnapshot)
+            throw Exceptions::CreateToolhelp32SnapshotException("CreateToolhelp32Snapshot failed to create a snapshot of modules.", GetLastError());
+
+        bool copiedToBuffer = Module32First(modulesSnapshot.get(), &module);
+        if(!copiedToBuffer)
+            throw Exceptions::Module32Exception("Module32First failed to fill the buffer.", GetLastError());
+
+        do {
+            modules.push_back(module);
+            copiedToBuffer = Module32Next(modulesSnapshot.get(), &module);
+        } while(copiedToBuffer);
+
+        if(GetLastError() != ERROR_NO_MORE_FILES)
+            throw Exceptions::Module32Exception("Module32Next failed to fill the buffer.", GetLastError());
+
+        return modules;
+    }
+
+    MODULEENTRY32W MemoryManager::GetModule(const std::wstring& moduleName) {
+        std::vector<MODULEENTRY32W> modules = GetModules();
+
+        for(auto currentModule : modules) {
+            if(boost::algorithm::iequals(moduleName, currentModule.szModule))
+                return currentModule;
+        }
+
+        throw std::runtime_error("Couldn't find a module with the specified name in the modules list.");
+    }
+
+    uintptr_t MemoryManager::GetModuleBaseAddress(const std::wstring& moduleName) {
+        auto module = GetModule(moduleName);
+
+        return uintptr_t(module.modBaseAddr);
+    }
+
+    size_t MemoryManager::GetModuleSize(const std::wstring& moduleName) {
+        const auto module = GetModule(moduleName);
+        return size_t(module.modBaseSize);
+    }
+
+    DWORD MemoryManager::GetProcessId() {
+        return _processId;
+    }
+
+    std::vector<BYTE> MemoryManager::ReadVirtualMemory(const std::vector<uintptr_t>& pointers, const size_t bytesNumber) {
+        const uintptr_t calculatedAddress = GetAddress(pointers);
+        std::vector<BYTE> byteSequence = ReadVirtualMemory(calculatedAddress, bytesNumber);
+
+        return byteSequence;
+    }
+
+    std::vector<BYTE> MemoryManager::ReadVirtualMemory(const uintptr_t baseAddress, const std::vector<uintptr_t>& offsets, const int bytesNumber) {
+        const uintptr_t calculatedAddress = GetAddress(baseAddress, offsets);
+        std::vector<BYTE> byteSequence = ReadVirtualMemory(calculatedAddress, bytesNumber);
+
+        return byteSequence;
+    }
+
+    std::vector<BYTE> MemoryManager::ReadVirtualMemory(const std::wstring& moduleName, const uintptr_t offset, const size_t bytesNumber) {
+        const uintptr_t calculatedAddress = GetAddress(moduleName, offset);
+        std::vector<BYTE> byteSequence = ReadVirtualMemory(calculatedAddress, bytesNumber);
+
+        return byteSequence;
+    }
+
+    std::vector<BYTE> MemoryManager::ReadVirtualMemory(const std::wstring& moduleName, const std::vector<uintptr_t>& offsets, const size_t bytesNumber) {
+        const uintptr_t calculatedAddress = GetAddress(moduleName, offsets);
+        std::vector<BYTE> byteSequence = ReadVirtualMemory(calculatedAddress, bytesNumber);
+
+        return byteSequence;
+    }
+
+    void MemoryManager::WriteVirtualMemory(const std::vector<uintptr_t>& pointers, const std::vector<byte>& byteSequence) {
+        const uintptr_t calculatedAddress = GetAddress(pointers);
+        WriteVirtualMemory(calculatedAddress, byteSequence);
+    }
+
+    void MemoryManager::WriteVirtualMemory(const uintptr_t baseAddress, const std::vector<uintptr_t>& offsets, const std::vector<byte>& byteSequence) {
+        const uintptr_t calculatedAddress = GetAddress(baseAddress, offsets);
+        WriteVirtualMemory(calculatedAddress, byteSequence);
+    }
+
+    void MemoryManager::WriteVirtualMemory(const std::wstring& moduleName, const uintptr_t offset, const std::vector<byte>& byteSequence) {
+        const uintptr_t calculatedAddress = GetAddress(moduleName, offset);
+        WriteVirtualMemory(calculatedAddress, byteSequence);
+    }
+
+    void MemoryManager::WriteVirtualMemory(const std::wstring& moduleName, const std::vector<uintptr_t>& offsets, const std::vector<byte>& byteSequence) {
+        const uintptr_t calculatedAddress = GetAddress(moduleName, offsets);
+        WriteVirtualMemory(calculatedAddress, byteSequence);
+    }
+
+    uintptr_t MemoryManager::GetAddress(const std::vector<uintptr_t>& pointers) {
+        auto pointerIterator = pointers.begin();
+        uintptr_t baseAddress = *pointerIterator;
+        ++pointerIterator;
+        uintptr_t address{};
+        while(pointerIterator < pointers.end() - 1) {
+            address = MemoryManager::ReadVirtualMemory<uintptr_t>(baseAddress + *pointerIterator);
+            baseAddress = address;
+
+            ++pointerIterator;
+        }
+
+        const uintptr_t endAddress = address + *pointers.end() - 1;
+
+        return endAddress;
+    }
+
+    uintptr_t MemoryManager::GetAddress(const uintptr_t baseAddress, std::vector<uintptr_t> offsets) {
+        offsets.insert(offsets.begin(), baseAddress);
+        const uintptr_t calculatedAddress = GetAddress(offsets);
+        return calculatedAddress;
+    }
+
+    uintptr_t MemoryManager::GetAddress(const std::wstring& moduleName, const uintptr_t offset) {
+        const uintptr_t baseAddress = GetModuleBaseAddress(moduleName);
+        const uintptr_t calculatedAddress = baseAddress + offset;
+
+        return calculatedAddress;
+    }
+
+    uintptr_t MemoryManager::GetAddress(const std::wstring& moduleName, std::vector<uintptr_t> offsets) {
+        const uintptr_t baseAddress = GetModuleBaseAddress(moduleName);
+        offsets.insert(offsets.begin(), baseAddress);
+        const uintptr_t calculatedAddress = GetAddress(offsets);
+
+        return calculatedAddress;
+    }
+
     template <typename TStructure>
-    TStructure MemoryManager::ReadVirtualMemory(uintptr_t baseAddress) {
+    TStructure MemoryManager::ReadVirtualMemory(const uintptr_t baseAddress) {
         std::vector<BYTE> byteSequence = ReadVirtualMemory(baseAddress, sizeof(TStructure));
         BYTE* bytePointer = &byteSequence[0];
         auto structure = reinterpret_cast<TStructure>(bytePointer);
@@ -10,7 +201,7 @@ namespace MemoryCommando::Memory {
     }
 
     template <typename TStructure>
-    TStructure MemoryManager::ReadVirtualMemory(std::vector<uintptr_t> pointers) {
+    TStructure MemoryManager::ReadVirtualMemory(const std::vector<uintptr_t>& pointers) {
         const uintptr_t calculatedAddress = GetAddress(pointers);
         TStructure structure = ReadVirtualMemory<TStructure>(calculatedAddress);
 
@@ -18,7 +209,7 @@ namespace MemoryCommando::Memory {
     }
 
     template <typename TStructure>
-    TStructure MemoryManager::ReadVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> offsets) {
+    TStructure MemoryManager::ReadVirtualMemory(const uintptr_t baseAddress, const std::vector<uintptr_t>& offsets) {
         const uintptr_t calculatedAddress = GetAddress(baseAddress, offsets);
         TStructure structure = ReadVirtualMemory<TStructure>(calculatedAddress);
 
@@ -26,7 +217,7 @@ namespace MemoryCommando::Memory {
     }
 
     template <typename TStructure>
-    TStructure MemoryManager::ReadVirtualMemory(std::wstring moduleName, uintptr_t offset) {
+    TStructure MemoryManager::ReadVirtualMemory(const std::wstring& moduleName, const uintptr_t offset) {
         const uintptr_t calculatedAddress = GetAddress(moduleName, offset);
         TStructure structure = ReadVirtualMemory<TStructure>(calculatedAddress);
 
@@ -34,7 +225,7 @@ namespace MemoryCommando::Memory {
     }
 
     template <typename TStructure>
-    TStructure MemoryManager::ReadVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> offsets) {
+    TStructure MemoryManager::ReadVirtualMemory(const std::wstring& moduleName, const std::vector<uintptr_t>& offsets) {
         const uintptr_t calculatedAddress = GetAddress(moduleName, offsets);
         TStructure structure = ReadVirtualMemory<TStructure>(calculatedAddress);
 
@@ -42,7 +233,7 @@ namespace MemoryCommando::Memory {
     }
 
     template <typename TStructure>
-    void MemoryManager::WriteVirtualMemory(uintptr_t baseAddress, TStructure structure) {
+    void MemoryManager::WriteVirtualMemory(const uintptr_t baseAddress, const TStructure& structure) {
         const size_t structureSize = sizeof(TStructure);
         std::vector<BYTE> structureByteSequence{};
 
@@ -54,25 +245,25 @@ namespace MemoryCommando::Memory {
     }
 
     template <typename TStructure>
-    void MemoryManager::WriteVirtualMemory(std::vector<uintptr_t> pointers, TStructure structure) {
+    void MemoryManager::WriteVirtualMemory(const std::vector<uintptr_t>& pointers, const TStructure& structure) {
         const uintptr_t calculatedAddress = GetAddress(pointers);
         WriteVirtualMemory(calculatedAddress, structure);
     }
 
     template <typename TStructure>
-    void MemoryManager::WriteVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> pointers, TStructure structure) {
+    void MemoryManager::WriteVirtualMemory(const uintptr_t baseAddress, const std::vector<uintptr_t>& pointers, const TStructure& structure) {
         const uintptr_t calculatedAddress = GetAddress(baseAddress, pointers);
         WriteVirtualMemory(calculatedAddress, structure);
     }
 
     template <typename TStructure>
-    void MemoryManager::WriteVirtualMemory(std::wstring moduleName, uintptr_t offset, TStructure structure) {
+    void MemoryManager::WriteVirtualMemory(const std::wstring& moduleName, const uintptr_t offset, const TStructure& structure) {
         const uintptr_t calculatedAddress = GetAddress(moduleName, offset);
         WriteVirtualMemory(calculatedAddress, structure);
     }
 
     template <typename TStructure>
-    void MemoryManager::WriteVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> offsets, TStructure structure) {
+    void MemoryManager::WriteVirtualMemory(const std::wstring& moduleName, const std::vector<uintptr_t>& offsets, const TStructure& structure) {
         const uintptr_t calculatedAddress = GetAddress(moduleName, offsets);
         WriteVirtualMemory(calculatedAddress, structure);
     }
--- MemoryCommando/Memory/MemoryManager.h
+++ MemoryCommando/Memory/MemoryManager.h
@@ -7,64 +7,69 @@
 
 #include "windows.h"
 #include "TlHelp32.h"
-#include <wil\resource.h>
+#include <wil/resource.h>
 
 namespace MemoryCommando::Memory {
     class MemoryManager {
     public:
         virtual ~MemoryManager() = default;
-        virtual PROCESSENTRY32W GetProcess() = 0;
+
+        virtual SYSTEM_INFO GetSystemInfo();
+        virtual std::vector<PROCESSENTRY32W> GetRunningProcesses();
+        virtual PROCESSENTRY32W GetProcess();
+        virtual std::wstring GetProcessName();
+        virtual std::vector<MODULEENTRY32W> GetModules() const;
+        virtual MODULEENTRY32W GetModule(const std::wstring& moduleName);
+        virtual uintptr_t GetModuleBaseAddress(const std::wstring& moduleName);
+        virtual size_t GetModuleSize(const std::wstring& moduleName);
+
         virtual DWORD GetProcessId() = 0;
-        virtual SYSTEM_INFO GetSystemInfo() = 0;
         virtual HANDLE GetProcessHandle() = 0;
-        virtual std::wstring GetProcessName() = 0;
-        virtual std::vector<MODULEENTRY32W> GetModules() const = 0;
-        virtual MODULEENTRY32W GetModule(const std::wstring& moduleName) = 0;
-        virtual uintptr_t GetModuleBaseAddress(const std::wstring& moduleName) = 0;
-        virtual size_t GetModuleSize(const std::wstring& moduleName) = 0;
+
         virtual uintptr_t AllocateVirtualMemory(uintptr_t baseAddress, size_t allocationSize, DWORD allocationType = MEM_RESERVE | MEM_COMMIT, DWORD protectionType = PAGE_EXECUTE_READWRITE) = 0;
         virtual void FreeVirtualMemory(uintptr_t baseAddress, DWORD freeType = MEM_RELEASE, size_t size = 0) = 0;
         virtual void ProtectVirtualMemory(uintptr_t baseAddress, size_t protectionSize, DWORD protectionType = PAGE_EXECUTE_READWRITE) = 0;
         virtual MEMORY_BASIC_INFORMATION QueryVirtualMemory(uintptr_t baseAddress) = 0;
 
         virtual std::vector<BYTE> ReadVirtualMemory(uintptr_t baseAddress, size_t bytesNumber) = 0;
-        virtual std::vector<BYTE> ReadVirtualMemory(std::vector<uintptr_t> pointers, size_t bytesNumber) = 0;
-        virtual std::vector<BYTE> ReadVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> offsets, int bytesNumber) = 0;
-        virtual std::vector<BYTE> ReadVirtualMemory(std::wstring moduleName, uintptr_t offset, size_t bytesNumber) = 0;
-        virtual std::vector<BYTE> ReadVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> offsets, size_t bytesNumber) = 0;
+        virtual std::vector<BYTE> ReadVirtualMemory(const std::vector<uintptr_t>& pointers, size_t bytesNumber);
+        virtual std::vector<BYTE> ReadVirtualMemory(uintptr_t baseAddress, const std::vector<uintptr_t>& offsets, int bytesNumber);
+        virtual std::vector<BYTE> ReadVirtualMemory(const std::wstring& moduleName, uintptr_t offset, size_t bytesNumber);
+        virtual std::vector<BYTE> ReadVirtualMemory(const std::wstring& moduleName, const std::vector<uintptr_t>& offsets, size_t bytesNumber);
         // todo ReadVirtualMemory from array of bytes and offset
 
-        virtual void WriteVirtualMemory(uintptr_t baseAddress, std::vector<byte> byteSequence) = 0;
-        virtual void WriteVirtualMemory(std::vector<uintptr_t> pointers, std::vector<byte> byteSequence) = 0;
-        virtual void WriteVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> offsets, std::vector<byte> byteSequence) = 0;
-        virtual void WriteVirtualMemory(std::wstring moduleName, uintptr_t offset, std::vector<byte> byteSequence) = 0;
-        virtual void WriteVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> offset, std::vector<byte> byteSequence) = 0;
+        virtual void WriteVirtualMemory(uintptr_t baseAddress, const std::vector<byte>& byteSequence) = 0;
+        virtual void WriteVirtualMemory(const std::vector<uintptr_t>& pointers, const std::vector<byte>& byteSequence);
+        virtual void WriteVirtualMemory(uintptr_t baseAddress, const std::vector<uintptr_t>& offsets, const std::vector<byte>& byteSequence);
+        virtual void WriteVirtualMemory(const std::wstring& moduleName, uintptr_t offset, const std::vector<byte>& byteSequence);
+        virtual void WriteVirtualMemory(const std::wstring& moduleName, const std::vector<uintptr_t>& offsets, const std::vector<byte>& byteSequence);
         // todo WriteVirtualMemory from array of bytes and offset
 
-        virtual uintptr_t GetAddress(std::vector<uintptr_t> pointers) = 0;
-        virtual uintptr_t GetAddress(uintptr_t baseAddress, std::vector<uintptr_t> pointers) = 0;
-        virtual uintptr_t GetAddress(std::wstring moduleName, uintptr_t offset) = 0;
-        virtual uintptr_t GetAddress(std::wstring moduleName, std::vector<uintptr_t> offsets) = 0;
+        virtual uintptr_t GetAddress(const std::vector<uintptr_t>& pointers);
+        virtual uintptr_t GetAddress(uintptr_t baseAddress, std::vector<uintptr_t> offsets);
+        virtual uintptr_t GetAddress(const std::wstring& moduleName, uintptr_t offset);
+        virtual uintptr_t GetAddress(const std::wstring& moduleName, std::vector<uintptr_t> offsets);
         // todo GetAddress from array of bytes and offset but add it to the MemoryCommander probably.
 
         template<typename TStructure> TStructure ReadVirtualMemory(uintptr_t baseAddress);
-        template<typename TStructure> TStructure ReadVirtualMemory(std::vector<uintptr_t> pointers);
-        template<typename TStructure> TStructure ReadVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> offsets);
-        template<typename TStructure> TStructure ReadVirtualMemory(std::wstring moduleName, uintptr_t offset);
-        template<typename TStructure> TStructure ReadVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> offsets);
+        template<typename TStructure> TStructure ReadVirtualMemory(const std::vector<uintptr_t>& pointers);
+        template<typename TStructure> TStructure ReadVirtualMemory(uintptr_t baseAddress, const std::vector<uintptr_t>& offsets);
+        template<typename TStructure> TStructure ReadVirtualMemory(const std::wstring& moduleName, uintptr_t offset);
+        template<typename TStructure> TStructure ReadVirtualMemory(const std::wstring& moduleName, const std::vector<uintptr_t>& offsets);
         // todo ReadVirtualMemory from array of bytes and offset
 
-        template<typename TStructure> void WriteVirtualMemory(uintptr_t baseAddress, TStructure structure);
-        template<typename TStructure> void WriteVirtualMemory(std::vector<uintptr_t> pointers, TStructure structure);
-        template<typename TStructure> void WriteVirtualMemory(uintptr_t baseAddress, std::vector<uintptr_t> pointers, TStructure structure);
-        template<typename TStructure> void WriteVirtualMemory(std::wstring moduleName, uintptr_t offset, TStructure structure);
-        template<typename TStructure> void WriteVirtualMemory(std::wstring moduleName, std::vector<uintptr_t> offsets, TStructure structure);
+        template<typename TStructure> void WriteVirtualMemory(uintptr_t baseAddress, const TStructure& structure);
+        template<typename TStructure> void WriteVirtualMemory(const std::vector<uintptr_t>& pointers, const TStructure& structure);
+        template<typename TStructure> void WriteVirtualMemory(uintptr_t baseAddress, const std::vector<uintptr_t>& pointers, const TStructure& structure);
+        template<typename TStructure> void WriteVirtualMemory(const std::wstring& moduleName, uintptr_t offset, const TStructure& structure);
+        template<typename TStructure> void WriteVirtualMemory(const std::wstring& moduleName, const std::vector<uintptr_t>& offsets, const TStructure& structure);
         // todo WriteVirtualMemory from array of bytes and offset
 
         // todo Commit, Reserve, Free memory functions that auto free memory on object destruction.
 
-    private:
-        wil::unique_handle _processHandle;
+    protected:
+        wil::unique_handle _processHandle = nullptr;
+        DWORD _processId = 0;
     };
 }
 
