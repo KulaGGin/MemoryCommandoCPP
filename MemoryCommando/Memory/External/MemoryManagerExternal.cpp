fbb64485826c69c8e549426134fa9ee2924fa98d
KulaGGin <sergeant.coolagin@gmail.com>
2020-07-10 21:11:54 +0300

    Refactor MemoryManagerExternal class.
    Move static methods to HelperMethods class.

--- MemoryCommando/Memory/External/MemoryManagerExternal.cpp
+++ MemoryCommando/Memory/External/MemoryManagerExternal.cpp
@@ -10,6 +10,7 @@
 #include "../../Exceptions/VirtualProtectExException.h"
 #include "../../Exceptions/VirtualQueryExException.h"
 #include "../../Exceptions/WriteProcessMemoryException.h"
+#include "../../HelperMethods.h"
 
 namespace MemoryCommando::Memory::External {
     MemoryManagerExternal::MemoryManagerExternal(const DWORD processId, const DWORD processAccess) {
@@ -19,7 +20,7 @@ namespace MemoryCommando::Memory::External {
     }
 
     MemoryManagerExternal::MemoryManagerExternal(const std::wstring& processName, const size_t processNumber, const DWORD processAccess) {
-        _process = GetProcess(processName, processNumber);
+        _process = HelperMethods::GetProcess(processName, processNumber);
         _processId = _process.th32ProcessID;
         _processHandle = wil::unique_handle(GetProcessHandle(processAccess));
     }
