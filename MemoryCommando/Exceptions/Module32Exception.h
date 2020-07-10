adc4d086c8e3e5de9901679353d436b0ec297362
KulaGGin <sergeant.coolagin@gmail.com>
2020-07-10 21:07:59 +0300

    Refactor Module32Exception.
    Remove unused using namespace.

--- MemoryCommando/Exceptions/Module32Exception.h
+++ MemoryCommando/Exceptions/Module32Exception.h
@@ -2,7 +2,6 @@
 #include "WinAPIException.h"
 
 namespace MemoryCommando::Exceptions {
-    using namespace MemoryCommando::Exceptions;
     class Module32Exception : WinAPIException{
     public:
         Module32Exception(const std::string& errorMessage, DWORD lastError);
