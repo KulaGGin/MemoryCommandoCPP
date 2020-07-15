71e4161ab5b12f6f058179d7e6b9f13e2bb37e70
KulaGGin <sergeant.coolagin@gmail.com>
2020-07-15 13:15:57 +0300

    Fill DetourCreator class.

--- MemoryCommando/Memory/DetourCreator.cpp
+++ MemoryCommando/Memory/DetourCreator.cpp
@@ -1,5 +1,45 @@
 #include "DetourCreator.h"
 
+#include <utility>
+
 namespace MemoryCommando {
-    
+
+    DetourCreator::DetourCreator(std::shared_ptr<const MemoryManager> memoryManager) :
+        _pageSize(HelperMethods::GetSystemInfo().dwPageSize),
+        _memoryManager{ std::move(memoryManager) } {
+    }
+
+    void DetourCreator::InjectCode(const uintptr_t injectionAddress, const size_t instructionLength, const std::vector<BYTE>& machineCode) const {
+        // Allocate memory
+        const size_t pageNumber = machineCode.size() / _pageSize + 1;
+        const size_t allocationSize = pageNumber * _pageSize;
+        const uintptr_t codeCaveAddress = _memoryManager->AllocateVirtualMemory(allocationSize);
+
+        _memoryManager->WriteVirtualMemory(codeCaveAddress, machineCode);
+
+        // Create injection trampoline
+        std::vector<BYTE> injectionTrampolineMachineCode{ _relativeJumpCode };
+        std::vector<BYTE> jumpOffsetBytes = HelperMethods::ConvertObjectToBytes(ptrdiff_t(codeCaveAddress - injectionAddress + _relativeJumpSize));
+        injectionTrampolineMachineCode.insert(injectionTrampolineMachineCode.end(), jumpOffsetBytes.begin(), jumpOffsetBytes.end());
+        AddNops(injectionTrampolineMachineCode, instructionLength);
+
+        // Calculate trampoline back to the address after original jump
+        std::vector<BYTE> codeCaveTrampolineMachineCode{ _relativeJumpCode };
+        const uintptr_t afterInjectionInstructionAddress = injectionAddress + instructionLength;
+        const uintptr_t afterCodeCaveAddress = codeCaveAddress + machineCode.size();
+        std::vector<BYTE> codeCaveTrampolineOffsetBytes = HelperMethods::ConvertObjectToBytes(ptrdiff_t(afterInjectionInstructionAddress - afterCodeCaveAddress + _relativeJumpSize));
+        codeCaveTrampolineMachineCode.insert(codeCaveTrampolineMachineCode.end(), codeCaveTrampolineOffsetBytes.begin(), codeCaveTrampolineOffsetBytes.end());
+
+        // Write trampoline in the code cave back to the original code after original jump at the injection address
+        _memoryManager->WriteVirtualMemory(afterCodeCaveAddress, codeCaveTrampolineMachineCode);
+
+        // Write trampoline in the original code(at the injection address) to the code cave
+        _memoryManager->WriteVirtualMemory(injectionAddress, injectionTrampolineMachineCode);
+    }
+
+    void DetourCreator::AddNops(std::vector<BYTE>& machineCode, const size_t neededLength) const {
+        for(size_t length = machineCode.size(); length < neededLength; ++length) {
+            machineCode.push_back(_nopCode);
+        }
+    }
 }
--- MemoryCommando/Memory/DetourCreator.h
+++ MemoryCommando/Memory/DetourCreator.h
@@ -1,10 +1,26 @@
 #pragma once
 
+#include <Memory/MemoryManager.h>
+#include <memory>
+
+#include "HelperMethods.h"
+
 namespace MemoryCommando {
+    using namespace Memory;
     class DetourCreator {
     public:
-
+        DetourCreator(std::shared_ptr<const MemoryManager> memoryManager);
     private:
+        const size_t _relativeJumpSize = 5;
+        const BYTE _relativeJumpCode = 0xE9;
+        const BYTE _nopCode = 0x90;
+        const size_t _pageSize;
+
+        const std::shared_ptr<const MemoryManager> _memoryManager;
+
+        void InjectCode(uintptr_t injectionAddress, size_t instructionLength, const std::vector<BYTE>& machineCode) const;
+
+        void AddNops(std::vector<BYTE>& machineCode, size_t neededLength) const;
     };
 }
 
